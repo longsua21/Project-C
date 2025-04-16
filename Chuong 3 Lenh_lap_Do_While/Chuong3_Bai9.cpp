a8c25ab81ef5c23d4df812ab6512ae9145b30374
longsua21 <mailanbn290775@gmail.com>
2025-04-16 22:02:56 +0700

    Chuong3_Bai9.cpp

--- Chuong 3 Lenh_lap_Do_While/Chuong3_Bai9.cpp
+++ Chuong 3 Lenh_lap_Do_While/Chuong3_Bai9.cpp
@@ -1,35 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    int thang, nam, songay;
+    int n, i, j;
+    double S = 1.0;  // bắt đầu từ 1
+    long long giaithua;
 
-    printf("Nhap thang (1-12): ");
-    scanf("%d", &thang);
-    printf("Nhap nam: ");
-    scanf("%d", &nam);
+    printf("Nhap so nguyen n: ");
+    scanf("%d", &n);
 
-    switch (thang) {
-        case 1: case 3: case 5: case 7:
-        case 8: case 10: case 12:
-            songay = 31;
-            break;
-        case 4: case 6: case 9: case 11:
-            songay = 30;
-            break;
-        case 2:
-            // Ki?m tra nam nhu?n
-            if ((nam % 4 == 0 && nam % 100 != 0) || (nam % 400 == 0))
-                songay = 29;
-            else
-                songay = 28;
-            break;
-        default:
-            printf("Thang khong hop le!\n");
-            return 0;
+    if (n < 1) {
+        printf("Vui long nhap n >= 1\n");
+        return 0;
     }
 
-    printf("Thang %d nam %d co %d ngay.\n", thang, nam, songay);
+    for (i = 2; i <= n; i++) {
+        giaithua = 1;
+        for (j = 1; j <= i; j++) {
+            giaithua *= j;
+        }
+        S += 1.0 / giaithua;
+    }
+
+    printf("Tong S = %.6lf\n", S);
 
     return 0;
 }
-
